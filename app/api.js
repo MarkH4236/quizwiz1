0fffb0c2a477f17395c4969e6b0710fa1df8d8e1
sc3rch <147206028+sc3rch@users.noreply.github.com>
2024-12-12 18:01:28 -0700

    added api to get and add to mockapi.io

---
+++ app/api.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const API_BASE_URL = "https://6736a06caafa2ef222310633.mockapi.io";
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+});
+
+export const addFlashcard = async (flashcard) => {
+  try {
+    const response = await api.post("/questionSet", flashcard);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getFlashcards = async () => {
+  try {
+    const response = await api.get("/questionSet");
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
